49a069fb81f7fa5e26b9b2afda72448d24509522
arun <arun@localhost.localdomain>
2016-04-07 12:02:35 +0200

    er-coap files as backup

---
+++ platform/lorafabian/apps/eap_responder/eap_responder_ec.c
@@ -0,0 +1,103 @@
+#include "contiki.h"
+#include <stdint.h> 
+#include "er-coap.h"
+#include "er-coap-constants.h"
+#include "eap-peer.c"
+#include "eap-psk.c"
+#include "eax.c"
+#include "eap_responder.h"
+#include "test.h"
+#include "layer802154_radio_lora.h"
+#include "frame_manager.h"
+//#include "_cantcoap.h"
+
+
+
+uint8_t authenticated = FALSE;
+static struct etimer et;
+uint8_t state;
+
+void eap_responder_init()
+{
+  process_start(&eap_responder_process, NULL); 
+}
+
+void eap_responder_sm_init() {
+
+  eapRestart = TRUE;
+  eap_peer_sm_step(NULL);
+}
+
+void eap_responder_timer_init() {
+  printf("\n\r eap_responder timer reset\n\r");
+  etimer_set(&et, 45*CLOCK_SECOND);
+}
+
+static void eventhandler(process_event_t ev, process_data_t data) {
+
+  int i;
+  coap_packet_t *p;
+  static coap_packet_t coap_response[1];
+  uint8_t tx_buffer[512];
+  size_t coap_packet_size;
+  int tx_buffer_index;
+
+  p = (coap_packet_t *)data;
+    
+  coap_init_message(coap_response, COAP_TYPE_ACK, 0, p->mid);
+  coap_set_status_code (coap_response, CHANGED_2_04);
+  if (!eapKeyAvailable) {
+       eapReq = TRUE;
+       eap_peer_sm_step (p->payload);
+       coap_set_payload(coap_response, eapRespData, NTOHS(((struct eap_msg *) eapRespData)->length));
+       coap_packet_size = coap_serialize_message(coap_response, (void *)(tx_buffer ));
+       tx_buffer_index = coap_packet_size; 
+  } else {
+       printf ("EAP EXCHANGE FINISHED\n\r");
+       // verify if its EAP method = 03, as in EAP Success!	
+       if (*(p->payload) == 0x03) {
+       	authenticated = TRUE;
+       }
+  } 
+  // send the coap msgs with the SIGNALIZATIO BIT set.
+  printf("WE ARE SENDING RESPONSE TO COAP PUT\n\r");
+  layer802154_send(tx_buffer, tx_buffer_index, GATEWAY_ADDR, SIGNALISATION_ON, DST_SHORT_FLAG);
+}
+
+static void timeout_handler() {
+  // if the node is not authenticated within this timeout interval;
+  // start responding to beacons again after the timeout
+  if(!state) {
+	authenticated = FALSE;
+	is_associated = 0;
+	printf("\n\rEAP PROCESS TIMED OUT!\n\r");
+  }
+  etimer_set(&et, 45*CLOCK_SECOND);
+  state = 0;
+}
+
+PROCESS(eap_responder_process, "EAP responder process");
+PROCESS_THREAD(eap_responder_process, ev, data)
+{
+  PROCESS_BEGIN();
+  etimer_set(&et, 1*CLOCK_SECOND);
+  state = 1;
+  while (1)
+    {
+      PROCESS_YIELD();
+
+      if(etimer_expired(&et)) {
+	timeout_handler();
+      } else if(ev == event_data_ready) {
+        eventhandler(ev, data);
+      } else if(ev == event_timeout) {
+	printf("\n\rEAP PROCESS timeout signalled\n\r");
+	eap_responder_timer_init();
+      }
+      if(authenticated) {
+	printf("\n\rEAP PROCESS EXITED\n\r");
+	PROCESS_EXIT();
+      }
+    }
+  PROCESS_END();
+}
---
+++ platform/lorafabian/apps/frame_manager/frame_manager_ec.c
@@ -0,0 +1,264 @@
+/*--------------------------------------------------------------------------
+
+Copyright (c) <2015>, <Wi6labs>, <Telecom Bretagne>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Telecom Bretagne, wi6labs nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL TELECOM BRETAGNE OR WI6LABS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+Description:
+LoRaFabian Beacon Answer code. LoRa RX setq sx1272 in receive mode on the
+channel configured in sx1272_contiki_radio.c
+
+-----------------------------------------------------------------------------*/                                                             
+
+#include "contiki.h"
+#include "leds-arch.h"
+#include "stm32f10x.h"
+#include <dev/leds.h>
+#include <stdio.h>
+#include "layer802154_radio_lora.h"
+#include "arduino_spi.h"
+#include "debug_on_arduino.h"
+#include "er-coap.h"
+#include "er-coap-constants.h"
+#include "frame_manager.h"
+#include "frame802154_lora.h"
+#include "cfs/cfs.h"
+#include <string.h>
+#include "eap_responder.h"
+//#include "_cantcoap.h"
+
+static struct etimer rx_timer;
+static struct etimer timer_payload_beacon;
+process_event_t event_data_ready;
+process_event_t event_timeout;
+
+//The response to the beacon
+char coap_payload_beacon[150];
+
+int is_associated = 0;
+static int is_beacon_receive = 0;
+
+void frame_manager_init()
+{
+  process_start(&lorafab_bcn_process, NULL);
+}
+
+/**
+ * \brief: update the hostname with /HOSTNAME file
+ */
+void updateHOSTNAME()
+{
+  char dns[150];//The content of the file
+  int fd;
+  //Read in /HOSTNAME_LORA
+  fd = cfs_open("/HOSTNAME_LORA", CFS_READ);
+  if(fd >= 0) {
+    //Read 500 char
+    cfs_read(fd, dns, sizeof(dns));
+    cfs_close(fd);
+    //Get the real hostname
+    int size = 0;
+    //Because the space significate the end of the hostname
+    while(dns[size] != '\0')
+      ++size;
+    //final = the real url
+    char final[size];
+    int i;
+    for(i = 0; i != sizeof(final) +1; ++i)
+      final[i] = dns[i];
+    strcpy(coap_payload_beacon, "{\"n\":\"");
+    strcat(coap_payload_beacon, final);
+    strcat(coap_payload_beacon, "\"}");
+  }
+  else {
+    printf("ERREUR LORS DE LA LECTURE\n\r");
+    strcpy(coap_payload_beacon, "{\"n\":\"default.test\"}");
+    return;
+  }
+  printf("HOSTNAME : %s\n\r", coap_payload_beacon);
+}
+
+
+/**
+ * \brief: Send the coap_payload_beacon to layer802154
+ */
+void
+coap_beacon_send_response() {
+  updateHOSTNAME();
+  uint8_t tx_buffer[512];
+
+  size_t coap_packet_size;
+  static coap_packet_t coap_request[1];      //This way the packet can be treated as pointer as usual.
+
+  unsigned short random_a = random_rand();
+  char MAC[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+  getMac(MAC);
+
+  unsigned short random_b = random_a ^ MAC[sizeof(MAC)-1]; //
+  coap_init_message(coap_request, COAP_TYPE_CON, COAP_POST, (coap_get_mid()+random_b)%65535);
+
+  // CoAP message Response we sent /no n the uri-path :
+  coap_set_header_uri_path(coap_request, "n/");
+
+  //CoAP message Response we set the payload
+  int sizeMSG = strlen(coap_payload_beacon);
+  coap_set_payload(coap_request, (uint8_t *)coap_payload_beacon, sizeMSG);
+
+  coap_packet_size = coap_serialize_message(coap_request, (void *)(tx_buffer ));
+  int tx_buffer_index = coap_packet_size;
+  printf("We are sending the response to the coap beacon\n\r");
+
+  //Note: We don't parse the beacon message yet, so the GATEWAY_ADDR
+  //is defined in frame802154_lora.c
+  layer802154_send(tx_buffer, tx_buffer_index, GATEWAY_ADDR, SIGNALISATION_ON, DST_SHORT_FLAG);
+
+  //Note: We don't parse the beacon message yet, so we assume that
+  //the node is registered after the first response
+  //TODO: implement State Machine
+  is_associated = 1;
+  is_beacon_receive = 0;   // start responding to beacon, upon timeout
+  eap_responder_sm_init(); // init eap_sm, upon timeout
+  process_post_synch(&eap_responder_process, event_timeout,NULL);
+
+}
+
+/**
+ * \brief: Check if the payload is a beacon payload
+ * \param: rx_msg, the payload
+ * \param: size, the payload length
+ */
+int respond_if_coap_beacon(u8 rx_msg[], int size) {
+  printf("\n\r\tPayload: ");
+  int iaux;
+  for(iaux = 0 ; iaux<size ; iaux++)
+    printf("%02x", rx_msg[iaux]);
+
+   //static declaration reduces stack peaks and program code size
+  static coap_packet_t coap_message[1]; //this way the packet can be treated as pointer as usual
+  erbium_status_code = NO_ERROR;
+
+  erbium_status_code =
+  coap_parse_message(coap_message, (void *)(rx_msg), size);
+  printf("\n\r\tPayload is CoAP Beacon?: ");
+  if(erbium_status_code == NO_ERROR) {
+    int check_coap_beacon = (coap_message->type == COAP_TYPE_NON) && ((coap_message->code == COAP_POST)
+                            && (coap_message->payload_len == 0));
+    if(check_coap_beacon){
+      printf("\n\r\tThis is the LoRA CoAP Beacon\n\r");
+      return 1;
+    } else if((coap_message->code == COAP_PUT)
+                            && (coap_message->payload_len != 0)
+				&& (authenticated != TRUE)) {
+        printf("EAP CoAP Message of len=%d.\n\r",coap_message->payload_len);
+	// post an event to EAP responder and pass the coap pckt
+	process_post_synch(&eap_responder_process, event_data_ready,coap_message);
+    } else {
+	printf("Other CoAP Message\n\r");
+    }
+  } else
+      printf("Not LoRa Beacon. CoAP parse ERROR\n\r");
+  printf("\n\r");
+  return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS(lorafab_bcn_process, "LoRaFabian Beacon process");
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(lorafab_bcn_process, ev, data) {
+  PROCESS_BEGIN();
+
+  int pending = 0;
+  int i;
+  int size;
+
+  leds_init();
+  leds_toggle(LEDS_ALL);
+  arduino_spi_init();
+  layer802154_init();
+  //Start infinite RX
+  layer802154_on();
+
+  etimer_set(&rx_timer, 5*CLOCK_SECOND);
+  event_data_ready = process_alloc_event();
+  event_timeout = process_alloc_event();
+
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    if(is_beacon_receive && !is_associated && etimer_expired(&timer_payload_beacon))
+    {
+      etimer_stop(&timer_payload_beacon);
+      coap_beacon_send_response();
+    }
+
+    if(etimer_expired(&rx_timer)) {
+      leds_toggle(LEDS_ALL);
+
+      pending = layer802154_pending_packet();
+      printf("pending_packet: %d\n\r", pending);
+
+      if(pending) {
+        frame802154_lora_t frame = layer802154_read();
+        size = frame.payload_len;
+
+        if(frame.header_len == -1)
+          printf("Error: buffer is too small for headers");
+        else {
+          //For the arduino
+          int packetSize = size + frame.header_len;
+          //Verify the destination of a message
+          bool br_msg = is_broadcast_addr(&frame);
+          bool my_mac = is_my_mac(&frame);
+          if(br_msg) {
+            printf("Broadcast message");
+            if(!is_signaling(&frame) || debug_on_arduino)
+              set_arduino_read_buf(frame.packet, packetSize);
+          }
+          else if(my_mac) {
+            printf("Message is for me");
+            set_arduino_read_buf(frame.payload, frame.payload_len);
+           }
+          else {
+            printf("Message is not for me");
+            if(debug_on_arduino)
+              set_arduino_read_buf(frame.packet, packetSize);
+          }
+
+          //To avoid collision
+          if(respond_if_coap_beacon(frame.payload, size) && !is_associated)
+          {
+            is_beacon_receive = 1;
+            //int random_timer = (random_rand()%30);
+            int random_timer = 1;
+            etimer_set(&timer_payload_beacon, random_timer*CLOCK_SECOND);
+          }
+        }
+      }
+      etimer_reset(&rx_timer);
+    }
+  }
+  PROCESS_END();
+}
+
+/*---------------------------------------------------------------------------*/
